aa2732d2cd8a5cb8d424a9e5c946af9ea3069c56
zsy <530822987@qq.com>
2023-09-11 00:14:13 +0800

    logging in Existing Users and create new ones

--- app/Http/Controllers/Auth/SocialAccountController.php
+++ app/Http/Controllers/Auth/SocialAccountController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SocialAccountController extends Controller
 {
@@ -14,8 +15,30 @@ class SocialAccountController extends Controller
 
     public function handleProviderCallback($provider)
     {
-        $user = Socialite::driver($provider)->user();
+        try{
+            $user = Socialite::driver($provider)->user();
+            $authUser = $this->findOrCreateUser($user, $provider);
+            Auth::login($authUser, true);
+        }catch(\Exception $e){
 
-        $user->token;
+            return redirect()->route('login');
+        }
+
+
+    }
+
+    private function findOrCreateUser($user, $provider)
+    {
+        $authUser = User::where('provider_id', $user->id)->first();
+        if($authUser){
+            return $authUser;
+        }
+
+        return User::create([
+            'name' => $user->name,
+            'email' => $user->email,
+            'provider_id' => $user->id,
+            'provider_name' => $provider,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\SocialAccountController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login/{provider}', [SocialAccountController::class, 'redirectToProvider'])->name('login.provider');
+Route::get('login/{provider}/callback', 'Auth\SocialAccountController@handleProviderCallback');
